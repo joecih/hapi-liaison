42a625a5de66842c0acfe1ee858056ff930c76ff
joecih <joecih@gmail.com>
2016-04-18 09:08:00 -0400

    Well I got this far :/, having never used Hapi I had to bypass routing for scripts. Note go back and review this

---
+++ public/maincontroller.js
@@ -0,0 +1,28 @@
+var myapp = angular.module('MyApp', []);
+
+myapp.controller('MainController', function($scope, $http) {
+
+  $scope.searchItem = '';
+
+  $scope.searchUser = function() {
+    $scope.searchItem = $scope.search
+      //console.log($scope.searchItem);
+
+    var _url = 'https://api.github.com/search/users?q=' + $scope.searchItem + '+repos:%3E42+followers:%3E1000';
+
+    try {
+      $http({
+        method: 'GET',
+        url: _url,
+      }).then(function successCallback(response) {
+        console.log(response);
+      }, function errorCallback(response) {
+        console.log("this didn't work!");
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+
+});
--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Path = require('path');
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public')
+      }
+    }
+  }
+});
 server.connection({
   port: 4000
 });
 
 server.register(require('inert'), (err) => {
+
+  if (err) {
+    throw err;
+  }
+
+
+
   server.route({
     method: 'GET',
-    path: '/',
-    handler: function(request, reply) {
-      reply.file('./public/index.html');
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+        index: true
+      }
     }
   });
+
 });
 
 
