7b68b095c6f05b3699ac232f7a79a1daffa9055e
joecih <joecih@gmail.com>
2016-04-18 12:19:20 -0400

    Updating gh-pages with latest grid change

---
+++ maincontroller0.js
@@ -0,0 +1,44 @@
+var myapp = angular.module('MyApp', []);
+
+myapp.controller('MainController', function($scope, $http) {
+
+  $scope.searchUser = function() {
+
+    console.log($scope.search);
+
+    if ($scope.search !== undefined) {
+      var _url = 'https://api.github.com/search/users?q=' + $scope.search; //+ '+repos:%3E42+followers:%3E1000';
+
+      try {
+        $http({
+          method: 'GET',
+          url: _url,
+        }).then(function successCallback(response) {
+          console.log(response.data.items.length);
+
+          if (response.data.items.length <= 0) {
+            alertMessage('[ WARNING ] : Your input value is not a valid user name');
+          } else {
+
+            $scope.items = response.data.items;
+            console.log(response.data.items);
+
+          }
+
+        }, function errorCallback(response) {
+          console.log("this didn't work!");
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      // Some error control here.
+      alertMessage("[ WARNING ] : You did not enter any text in the input field!");
+    }
+  }
+
+  function alertMessage(_message) {
+    alert(_message);
+  }
+
+});
