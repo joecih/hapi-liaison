7400b5325912ebfa58e940b8f46df676974aadbe
joecih <joecih@gmail.com>
2016-04-18 10:15:23 -0400

    addiing in some error control to the search input

--- public/maincontroller.js
+++ public/maincontroller.js
@@ -2,34 +2,33 @@ var myapp = angular.module('MyApp', []);
 
 myapp.controller('MainController', function($scope, $http) {
 
-  $scope.searchItem = '';
-
   $scope.searchUser = function() {
-    $scope.searchItem = $scope.search
-      //console.log($scope.searchItem);
 
-    var _url = 'https://api.github.com/search/users?q=' + $scope.searchItem; //+ '+repos:%3E42+followers:%3E1000';
+    if ($scope.search !== '' || $scope.search !== undefined) {
+      var _url = 'https://api.github.com/search/users?q=' + $scope.searchItem; //+ '+repos:%3E42+followers:%3E1000';
 
-    try {
-      $http({
-        method: 'GET',
-        url: _url,
-      }).then(function successCallback(response) {
-        console.log(response.data);
+      try {
+        $http({
+          method: 'GET',
+          url: _url,
+        }).then(function successCallback(response) {
+          console.log(response.data);
 
-        //$scope.user = $scope.searchItem;
-        $scope.items = response.data.items;
-        //$scope.avatar = response.data.items[0].avatar_url;
-        //console.log(response.data.items[0].avatar_url);
+          //$scope.user = $scope.searchItem;
+          $scope.items = response.data.items;
+          //$scope.avatar = response.data.items[0].avatar_url;
+          //console.log(response.data.items[0].avatar_url);
 
 
-      }, function errorCallback(response) {
-        console.log("this didn't work!");
-      });
-    } catch (e) {
-      console.log(e);
+        }, function errorCallback(response) {
+          console.log("this didn't work!");
+        });
+      } catch (e) {
+        console.log(e);
+      }
     }
+  } else {
+    // Some error control here.
   }
-
 
 });
