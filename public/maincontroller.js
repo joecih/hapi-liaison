3cac388534fce31e17ab4b6a4c05c93015af92b3
joecih <joecih@gmail.com>
2016-04-18 10:23:18 -0400

    Updated error check for empty input field

--- public/maincontroller.js
+++ public/maincontroller.js
@@ -4,8 +4,10 @@ myapp.controller('MainController', function($scope, $http) {
 
   $scope.searchUser = function() {
 
-    if ($scope.search !== '' || $scope.search !== undefined) {
-      var _url = 'https://api.github.com/search/users?q=' + $scope.searchItem; //+ '+repos:%3E42+followers:%3E1000';
+    console.log($scope.search);
+
+    if ($scope.search !== undefined) {
+      var _url = 'https://api.github.com/search/users?q=' + $scope.search; //+ '+repos:%3E42+followers:%3E1000';
 
       try {
         $http({
@@ -26,9 +28,10 @@ myapp.controller('MainController', function($scope, $http) {
       } catch (e) {
         console.log(e);
       }
+    } else {
+      // Some error control here.
+      alert("[ WARNING ] : You did not enter any text in the input field!");
     }
-  } else {
-    // Some error control here.
   }
 
 });
